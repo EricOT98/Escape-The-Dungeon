f81ee4c7f1cfdd09af4b06e952947147b9dd7f88
EricOT98 <EricOToole@Outlook.com>
2017-12-10 20:06:10 +0000

    Menu FUnctionality implemented using callbacks

--- ProjectJohnEric/Menu/MenuHandler.cpp
+++ ProjectJohnEric/Menu/MenuHandler.cpp
@@ -4,40 +4,88 @@ MenuHandler::MenuHandler()
 {
 }
 
-void MenuHandler::addMenu(MenuStates & state,std::unique_ptr<Menu> &m)
+void MenuHandler::addMenu(std::unique_ptr<Menu> &m)
 {
-	m->setMenuState(state);
 	m_menus.push_back(std::move(m));
 }
 
 void MenuHandler::update(Xbox360Controller & controller)
 {
-	m_menus.at(static_cast<int>(m_menuStates))->update(controller);
+	for (m_currentMenu = 0; m_currentMenu < m_menus.size(); ++m_currentMenu)
+	{
+		if (m_menus.at(m_currentMenu)->getMenuState() == m_menuState)
+		{
+			m_menus.at(m_currentMenu)->update(controller); //update current menu
+			if (m_nextState == m_menuState)
+			{
+				m_nextState = m_menus.at(m_currentMenu)->getNextState();
+			}
+			else
+			{
+				m_menuState = m_nextState;
+				m_menus.at(m_currentMenu)->resetNextGameState();
+			}
+			break;
+		}
+	}
 }
 
 void MenuHandler::render(sf::RenderWindow & window)
 {
-	m_menus.at(static_cast<int>(m_menuStates))->render(window);
-}
-
-void MenuHandler::goToNext()
-{
-	if (m_menus.at(static_cast<int>(m_menuStates))->getActive()) {
+	/*for (int i = 0; i < m_menus.size(); ++i)
+	{
+		if (m_menuState == m_menus.at(i)->getMenuState() && 
+			m_menus.at(i)->getActive())
+		{
+			m_menus.at(i)->render(window);
+		}
+	}*/
+	if (m_currentMenu < m_menus.size())
+	{
+		m_menus.at(m_currentMenu)->render(window);
 	}
 }
 
-bool MenuHandler::goToMenu(MenuStates & state)
+bool MenuHandler::setActive(MenuStates state)
 {
-	if (nullptr != m_menus.at(static_cast<int>(state)) )
+	for (int i = 0; i < m_menus.size(); ++i)
+	{
+		if (m_menus.at(i) != nullptr)
+		{
+			if (state == m_menus.at(i)->getMenuState())
+			{
+				m_menus.at(i)->setActive(true);
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+
+bool MenuHandler::goToMenu(MenuStates state)
+{
+	/*if (nullptr != m_menus.at(static_cast<int>(state)) )
 	{
 		m_menus.at(static_cast<int>(state))->setActive(true);
-		m_menus.at(static_cast<int>(m_menuStates))->setActive(false);
+		m_menus.at(static_cast<int>(m_menuState))->setActive(false);
 		return true;
+	}
+	return false;*/
+
+	for (int i = 0; i < m_menus.size(); i++)
+	{
+		if (state == m_menus.at(i)->getMenuState())
+		{
+			m_menuState = state;
+			m_nextState = m_menus.at(i)->getNextState();
+			m_menus.at(i)->setActive(true);
+		}
 	}
 	return false;
 }
 
 MenuStates MenuHandler::getMenuState()
 {
-	return m_menuStates;
+	return m_menuState;
 }
